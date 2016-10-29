3f034f932483b41a1f3e6202e137ee69ec00ccee
Aaron Thomas <1d20m6@gmail.com>
2016-10-29 15:39:45 -0400

    implemented hyphenate_phrase() and partially implemented phrase_break()

--- src/language_analysis.py
+++ src/language_analysis.py
@@ -1,5 +1,34 @@
+#!/usr/bin/python2
+'''
 from pycorenlp import StanfordCoreNLP
 nlp = StanfordCoreNLP('http://localhost:9001') #it's over NINE THOUSAND!
+i might not use this
+'''
+from nltk.corpus import cmudict
+
+d = cmudict.dict()
+
+def hyphenate_phrase(word):
+	return [len(list(y for y in x if y[-1].isdigit())) for x in d[word.lower()]]
+#this is from http://stackoverflow.com/a/4103234
+#thank you internet stranger for writing code that does what I want
+#i renamed the function to hyphenate_phrase 
+
+
+#takes a really long string and then returns a list. every element in the list
+def phrase_break(gan): #gan is an acroym for "Good Argument Name"
+	phrases = []
+
+	while len(phrases) > 0:
+		current_phrase = ''
+		next_index = '//todo(aaron) WRITE CODE TO DO THIS'#find beginning of next phrase, save its location in next_index
+		current phrase
+		#set cuttent_phrase to be a substring of 0 to the next beginning
+		#add it to phrases
+		#remove the current_phrase from gan
+		eocp = gen.rfind(current_phrase) #end of current phrase
+		gan = gan[eocp:]
+	return phrases
 
 '''
 language analysis for nightingale
@@ -14,6 +43,7 @@ determine which syllables are stressed (do this before rhyme stuff)
 pseudocode of this
 isolate the phrases
 identiy rhymes at ends of phrases
+find most important word in each sentence
 
 ALRIGHT SO CURRENT PLAN...
 get the stuff
