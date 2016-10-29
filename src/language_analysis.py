aba97a2e0892f423e5eb898c96a231ef19a6482e
Aaron Thomas <1d20m6@gmail.com>
2016-10-29 18:32:16 -0400

    added some code to language_anaysis.py it's getting close to completion, just need to write a regex statement and then implement a function

--- src/language_analysis.py
+++ src/language_analysis.py
@@ -5,39 +5,71 @@ nlp = StanfordCoreNLP('http://localhost:9001') #it's over NINE THOUSAND!
 i might not use this
 '''
 from nltk.corpus import cmudict
+import re
+import inflect
 
 d = cmudict.dict()
+p = inflect.engine()
+#p.number_to_words(99) returns "ninety-nine"
 
-def hyphenate_phrase(word):
+def word_syllable_count(word):
 	return [len(list(y for y in x if y[-1].isdigit())) for x in d[word.lower()]]
 #this is from http://stackoverflow.com/a/4103234
 #thank you internet stranger for writing code that does what I want
-#i renamed the function to hyphenate_phrase 
+#i renamed the function to word_syllable_count
 
+def phrase_syllable_count(phrase):
+	total = 0
+	for word in phrase.split(" "):
+		total = total + word_syllable_count(word)
+	return total
+
+#this takes a word and separates its syllables so that they are hyphenated
+#also capitalized
+def hyphenate_word(word):
+	pass #//@todo(aaron) write this
+
+#returns a hyphenated version
+def hyphenate_phrase(phrase):
+	words = phrase.split()
+	for word in words:
+		word = hyphenate_word(word)
+	return ''.join(words)
 
 #takes a really long string and then returns a list. every element in the list
 def phrase_break(gan): #gan is an acroym for "Good Argument Name"
 	phrases = []
+	phrase_finder = "//todo(aaron) make this"
+	number_finder = "[0-9]+"
 
+	numbers_found = re.findall(number_finder,gan)
+	for number in numbers_found:
+		gan = gan.replace(number,p.number_to_words(number),1)
+	#	yes i did it
+	#yatta
+	#numbers get changed into words
+
+	"""
+	#so this loop might be mostly redundant
 	while len(phrases) > 0:
 		current_phrase = ''
-		next_index = '//todo(aaron) WRITE CODE TO DO THIS'#find beginning of next phrase, save its location in next_index
-		current phrase = gan[:next_index]
+		next_index = rm.match(phrase_finder,gan)#find beginning of next phrase, save its location in next_index
+		current_phrase = gan[:next_index]
 
-		#set cuttent_phrase to be a substring of 0 to the next beginning
-		#add it to phrases
-		#remove the current_phrase from gan
-		eocp = gen.rfind(current_phrase) #end of current phrase
-		gan = gan[eocp:]
-		#these two lines of code might be redundant (in a bad way)
-		#gan = gan[next_index:] #I THINK THIS LINE DOES WHAT THE ABOVE TWO DO
+
+		'''
+		#we might not want to do this, find a way to process a phrase consisting of two subphrases
+		if current_phrase.find(',') != -1:#if a phrase has two subphrases
+			current_phrase = current_phrase.split(',')
+		'''#yeah i'm not gonna do that for now
+
+		gan = gan[next_index:] #might need to do a +1 or a -1. figure it out once I'm ready to test this function
+	
+	#more on tha loop being redundant
+	'''"""
+	phrases = re.split(phrase_finder,gan) #this line of code probably could take that loop's place
 	return phrases
 
-#takes a really long string, breaks it into phrases, and then hyphenates the phrases
-	def break_and_hyphenate(gan):
-		phrases = phrase_break(gan)
-		for phrase in phrases:
-			phrase = hyphenate_phrase(phrase)
 '''
 language analysis for nightingale
 https://github.com/mjlitz/nightingale is the main branch
@@ -56,7 +88,7 @@ find most important word in each sentence
 ALRIGHT SO CURRENT PLAN...
 get the stuff
 break it into phrases in list form
-['phrase1','phrase2',['subphrase1','subphrase2'],'phrase4']
+['phrase1','phrase2','phrase3','phrase4']
 figure out syllable stressing
 identify nearby phrases (5 apart?) that rhyme
 '''
