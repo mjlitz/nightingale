aae35c1d2c3151374484e58b98da2f3cf9326ee0
mjlitz <mjlitzster@gmail.com>
2016-10-29 13:13:11 -0400

    basic file writing

---
+++ src/nightingale.py
@@ -0,0 +1,25 @@
+'''NIGHTINGALE version 0.0
+
+...................
+..  NIGHTINGALE  ..
+...................
+
+Written by Matt Litzsinger, Andrew Huang, and Aaron Thomas
+Updated October 30, 2016
+
+
+
+'''
+
+#the range of the music that's created
+r = ['G,','A,','B,', C, D, E, F, G, A, B, c, d, e, f, g]
+f = open('out.txt','w')
+f.write('X: 1\nT: Nightingale #1\n')
+f.write('M: 4/4\nL: 1/4\nK: C\n')
+f.write(r[0])
+
+
+f.close()
+
+#def phrase():
+
