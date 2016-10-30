60e32a1ec8fbf8888b13c193057b91b8c47a4b33
Aaron Thomas <1d20m6@gmail.com>
2016-10-30 04:12:53 -0400

    ok we're gonna use pygame. that is the new plan for playing sound!

--- src/player.py
+++ src/player.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 import pysynth_e as pse
+import wave
 
 #takes in abc and wavifies it
 #abc is a tuple in abc notation that has data this is meant to process
 #name will be the name of the file
 def abc_wavify(abc,name):
 	pse.make_wav(abc,fn = "{}.wav".format(name))
+
+def play_wav(file):
+	pass#write this
