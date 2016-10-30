514fa674f17e8d1870690c8012663039059e4016
mjlitz <mjlitzster@gmail.com>
2016-10-30 00:25:31 -0400

    Add files via upload

--- src/nightingale.py
+++ src/nightingale.py
@@ -17,7 +17,8 @@ import math
 #the range of the music that's created
 r = ['C,','D,','E,','F,','G,','A,','B,', 'C', 'D', 'E', 'F', 'G', 'A', 'B', 'c', 'd', 'e', 'f', 'g']
 #the chance of various intervals. Should add to 1.0. [up 2nd, down 2nd, up 3rd, down 3rd...] 
-
+#               tonic med dom
+superphrase_start = [0.7,0.98,1.00]
 #        1    2nd  3rd  4th  5th  6th   7th   octave
 prob = [0.10,0.75,0.95,0.97,0.99,0.997,0.998,1.000]
 # data is number of phrases contained within
@@ -29,11 +30,13 @@ f.write('X: 1\nT: Nightingale #1\n')
 f.write('M: 4/4\nL: 1/4\nK: G\n')
 
 length = 100
-syllables = 10
-pup = 0.6
+pup = 0.55
+
+previous_interval = 0
+up = 0 #up = 1, down = 0
 	##current note
 key = 4
-dfromtonic = 0
+#dfromtonic = 0
 
 f.write(r[key])
 current = key
@@ -42,6 +45,14 @@ def parse_phrase(phrase, current, key):
 	for note in range(0,phrases[phrase]):
 		dfromtonic = current-key
 		rand = random.random()
+## Factors that affect pup and prob
+## 1. Current range. If we are low in the range we want to go higher, if higher we want to go lower
+## 2. Relation to tonic and where we are in the phrase. If we're in the middle of the phrase, we want
+##    to be higher in the range, then settle down to the tonic by the end of the phrase or superphrase.
+## 3. If we are doing a lot of the same interval for too long, we want to do something else. Decrease the
+##    probability of an interval after you repeat. Then reset back to default.
+		pup = (20-current)/20.0
+#		f.write('pup = '+str(pup)+'\n')
 		i = 0
 		while(i<len(prob)):
 			if rand <= prob[i]:
@@ -65,6 +76,7 @@ def parse_superphrase(superphrase, current, key):
 	phrase = 0
 	while (phrase < superphrases[superphrase]):
 		parse_phrase(phrase, current, key)
+		f.write('|')
 		phrase += 1
 
 parse_superphrase(0, current, key)
