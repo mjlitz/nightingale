21379f676effa7e66b62f37c0c3500d918a741d1
mjlitz <mjlitzster@gmail.com>
2016-10-30 04:43:39 -0400

    connected everything together. like a gale in the night.

--- src/nightingale.py
+++ src/nightingale.py
@@ -15,9 +15,10 @@ import random
 import math
 import language_analysis as la
 import lyrics
+import player as pl
 
 #the range of the music that's created
-r = ['g3','a3','b3','c','d','e','f','g','a','b','c5','d5','e5']
+r = ['C,','D,','E,','F,','G,','A,','B,', 'C', 'D', 'E', 'F', 'G', 'A', 'B', 'c', 'd', 'e', 'f', 'g']
 #the chance of various intervals. Should add to 1.0. [up 2nd, down 2nd, up 3rd, down 3rd...] 
 #               tonic med dom
 superphrase_start = [0.7,0.98,1.00]
@@ -25,18 +26,20 @@ superphrase_start = [0.7,0.98,1.00]
 prob = [0.10,0.75,0.95,0.97,0.99,0.997,0.998,1.000]
 # data is number of phrases contained within
 
-lyric_lines = la.phrase_break(lyrics.firework)
+lyric_lines = la.phrase_break(lyrics.rick)
 syl_count = []
 for i in lyric_lines:
 	syl_count.append(la.phrase_syllable_count(i))
+if syl_count[0] != 0:
+	syl_count = [0] + syl_count
 syl_count.append(0)
 print('len sylcount = '+str(len(syl_count)))
 print(syl_count)
 syl_pointer = 1
 
-f = open('data.txt','w')
+f = open('data.abc','w')
 f.write('X: 1\nT: Nightingale #1\n')
-f.write('M: 4/4\nL: 1/4\nK: G\n')
+f.write('M: 4/4\nL: 1/8\nK: G\n')
 
 pup = 0.55
 
@@ -52,10 +55,12 @@ previous_interval = 0
 up = 0 #up = 1, down = 0
 	##current note
 key = 4
-#dfromtonic = 0
-
-f.write('(\''+str(r[key])+'\', 4),')
+#f.write(r[key])
 current = key
+
+def write_note_length(i,syllables):
+	if i == syllables-1 and syllables < 12:
+		f.write('4')
 
 def parse_phrase(phrase, current, key):
 	note = 0
@@ -69,7 +74,7 @@ def parse_phrase(phrase, current, key):
 ##    to be higher in the range, then settle down to the tonic by the end of the phrase or superphrase.
 ## 3. If we are doing a lot of the same interval for too long, we want to do something else. Decrease the
 ##    probability of an interval after you repeat. Then reset back to default.
-		pup = (20-current)/20.0
+		pup = (15-current)/15.0
 #		f.write('pup = '+str(pup)+'\n')
 		i = 0
 		while(i<len(prob)):
@@ -83,12 +88,13 @@ def parse_phrase(phrase, current, key):
 				#f.write('current = '+str(current)+'\n')
 				#f.write('added = '+str(added)+'\n')
 				#f.write(str(n)+'\n')
-				f.write('(\''+str(r[current])+'\', 4)')
+				f.write(r[current])
+				write_note_length(note,syl_count[phrase])
 				break  
 			else:
 				i += 1
-		f.write('|')
 		note += 1
+	f.write('|')
 
 def find_block(syl_pointer):
 	begin = syl_pointer
@@ -104,11 +110,8 @@ def parse_superphrase(begin, length, current, key):
 	phrase = begin
 	while (phrase < begin+length):
 		parse_phrase(phrase, current, key)
-<<<<<<< HEAD
 		f.write('\n')
-=======
-		f.write('|')
->>>>>>> 11bb3ca2154c54ecc0c9f7519c245f16aaeb40c9
+		#f.write('|')
 		phrase += 1
 
 while (syl_pointer < len(syl_count)):
@@ -119,6 +122,8 @@ while (syl_pointer < len(syl_count)):
 	syl_pointer += length + 1
 
 
+pl.abc_wavify(data.abc,'rick')
+pl.play_wav('~/Music/nightingale/rick.wav')
 f.close()
 
 '''for phrase in range(0,len(phrases)):
--- src/player.py
+++ src/player.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python
 import pysynth_e as pse
-
+import wave
+import pygame
+pygame.init()
 #takes in abc and wavifies it
 #abc is a tuple in abc notation that has data this is meant to process
 #name will be the name of the file
 def abc_wavify(abc,name):
-	pse.make_wav(abc,fn = "{}.wav".format(name))
+	pse.make_wav(abc,fn = "~/Music/nightingale/{}.wav".format(name))
+
+#if this works then it means YEE
+def play_wav(filepath):
+	s = pygame.mixer.Sound(filepath)
+	s.start()
+	s.stop()
